20f61bbdb27d6338da39431e1681e0c843be842d
Bruce Han <intrager@naver.com>
2022-02-07 02:12:51 +0900

    section_4: Add object factory

--- moviebuddy/src/test/java/moviebuddy/domain/MovieFinderTest.java
+++ moviebuddy/src/test/java/moviebuddy/domain/MovieFinderTest.java
@@ -2,27 +2,28 @@ package moviebuddy.domain;
 
 import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import moviebuddy.MovieBuddyApplication;
+import moviebuddy.MovieBuddyFactory;
 
 /**
  * @author springrunner.kr@gmail.com
  */
 public class MovieFinderTest {
-
-	public static void main(String[] args) {
-		MovieFinder movieFinder = new MovieFinder();
+	MovieBuddyFactory movieBuddyFactory = new MovieBuddyFactory();	
+	MovieFinder movieFinder = new MovieFinder(new CsvMovieReader());	
 	
-		List<Movie> result = movieFinder.directedBy("Michael Bay");
-		assertEquals(3, result.size());
-
-        result = movieFinder.releasedYearBy(2015);
-        assertEquals(225, result.size());
+	@Test
+	void NotEmpty_directedBy() {
+		List<Movie> movies = movieFinder.directedBy("Michael Bay");
+		Assertions.assertEquals(3, movies.size());
 	}
 	
-	static void assertEquals(long expected, long actual) {
-		if (expected != actual) {
-			throw new RuntimeException(String.format("actual(%d) is different from the expected(%d)", actual, expected));			
-		}
-	}
-	
+	@Test
+	void NotEmpty_ReleasedYearBy() {
+		List<Movie> movies = movieFinder.releasedYearBy(2015);
+		Assertions.assertEquals(225,movies.size());
+	}	
 }

e51ebcc44560d81e150a6410ba193f1512b74dbd
Bruce Han <intrager@naver.com>
2022-03-18 05:48:52 +0900

    section_21: Use message-source interfaces for internationalization

--- moviebuddy/src/main/java/moviebuddy/MovieBuddyApplication.java
+++ moviebuddy/src/main/java/moviebuddy/MovieBuddyApplication.java
@@ -1,40 +1,48 @@
 package moviebuddy;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
 
-import moviebuddy.data.CsvMovieReader;
 import moviebuddy.domain.Movie;
 import moviebuddy.domain.MovieFinder;
-import moviebuddy.util.FileSystemUtils;
 
 /**
  * @author springrunner.kr@gmail.com
  */
+@Configuration
+@PropertySource("classpath:/messages.properties")	// classpath:는 생략 가능
 public class MovieBuddyApplication {
 
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		messageSource.setDefaultEncoding("utf-8");
+		messageSource.setCacheSeconds(5);
+		return messageSource;
+	}
+	
     public static void main(String[] args) throws Exception {
         new MovieBuddyApplication().run(args);
     }
@@ -45,12 +53,14 @@ public class MovieBuddyApplication {
      * (완료) 1. XML 문서로 작성된 영화 메타데이터도 다룰 수 있게 기능을 확장하라
      * (완료) 2. 영화 메타데이터 위치를 변경할 수 있도록 하라
      * (완료) 3. 영화 메타데이터 읽기 속도를 빠르게 하라
-     * TODO 4. 시스템 언어설정에 따라 애플리케이션 메시지가 영어 또는 한글로 출력되게 하라
+     * (완료) 4. 시스템 언어설정에 따라 애플리케이션 메시지가 영어 또는 한글로 출력되게 하라
      */
 
     public void run(String[] args) throws Exception {
-    	final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MovieBuddyFactory.class);
-    	
+    	final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+    			MovieBuddyFactory.class, MovieBuddyApplication.class);
+    	final Environment environment = applicationContext.getEnvironment();
+    	final MessageSource messageSource = applicationContext.getBean(MessageSource.class);
     	final MovieFinder movieFinder = applicationContext.getBean(MovieFinder.class);
 
         final AtomicBoolean running = new AtomicBoolean(true);
@@ -63,7 +73,7 @@ public class MovieBuddyApplication {
         final Map<Command, Consumer<List<String>>> commandActions = new HashMap<>();
         // 애플리케이션 종료:: ❯ quit
         commandActions.put(Command.Quit, arguments -> {
-            output.println("quit application.");
+            output.println(messageSource.getMessage("application.commands.quit", new Object[0], Locale.getDefault()));
             running.set(false);
         });
         // 감독으로 영화 검색:: ❯ directedBy Michael Bay
@@ -75,17 +85,19 @@ public class MovieBuddyApplication {
             List<Movie> moviesDirectedBy = movieFinder.directedBy(director);
             AtomicInteger counter = new AtomicInteger(1);
 
-            output.println(String.format("find for movies by %s.", director));
+            output.println(messageSource.getMessage("application.commands.directedBy", new Object[] { director }, Locale.getDefault()));
             moviesDirectedBy.forEach(it -> {
-                String data = String.format("%d. title: %-50s\treleaseYear: %d\tdirector: %-25s\twatchedDate: %s", counter.getAndIncrement(), it.getTitle(), it.getReleaseYear(), it.getDirector(), it.getWatchedDate().format(Movie.DEFAULT_WATCHED_DATE_FORMATTER));
+            	String format = messageSource.getMessage("application.commands.directedBy.format", new Object[0], Locale.getDefault());
+				String data = String.format(format, counter.getAndIncrement(), it.getTitle(), it.getReleaseYear(), it.getDirector(), it.getWatchedDate().format(Movie.DEFAULT_WATCHED_DATE_FORMATTER));
                 output.println(data);
             });
-            output.println(String.format("%d movies found.", moviesDirectedBy.size()));
+            output.println(messageSource.getMessage("application.commands.directedBy.count", new Object[] { moviesDirectedBy.size() }, Locale.getDefault()));
         });
         // 개봉년도로 영화 검색:: ❯ releasedYearBy 2015
         commandActions.put(Command.releasedYearBy, arguments -> {
             int releaseYear;
             try {
+            	// 사용자가 입력한 명령어를 해석
                 releaseYear = Integer.parseInt(arguments.get(1));
             } catch (IndexOutOfBoundsException | NumberFormatException error) {
                 throw new ApplicationException.InvalidCommandArgumentsException(error);
@@ -93,19 +105,21 @@ public class MovieBuddyApplication {
             List<Movie> moviesReleasedYearBy = movieFinder.releasedYearBy(releaseYear);
             AtomicInteger counter = new AtomicInteger(1);
 
-            output.println(String.format("find for movies from %s year.", releaseYear));
+            output.println(messageSource.getMessage("application.commands.releasedYearBy", new Object[] { String.valueOf(releaseYear) }, Locale.getDefault()));
             moviesReleasedYearBy.forEach(it -> {
-                String data = String.format("%d. title: %-50s\treleaseYear: %d\tdirector: %-25s\twatchedDate: %s", counter.getAndIncrement(), it.getTitle(), it.getReleaseYear(), it.getDirector(), it.getWatchedDate().format(Movie.DEFAULT_WATCHED_DATE_FORMATTER));
+            	String format = messageSource.getMessage("application.commands.releasedYearBy.format", new Object[0], Locale.getDefault());
+                String data = String.format(format, counter.getAndIncrement(), it.getTitle(), it.getReleaseYear(), it.getDirector(), it.getWatchedDate().format(Movie.DEFAULT_WATCHED_DATE_FORMATTER));
                 output.println(data);
             });
-            output.println(String.format("%d movies found.", moviesReleasedYearBy.size()));
+            output.println(messageSource.getMessage("application.commands.releasedYearBy.count", new Object[] { String.valueOf(moviesReleasedYearBy.size()) }, Locale.getDefault()));
         });
+        
 
         /*--------------------------------------------------------------------------------------*/
         /* 사용자가 입력한 값을 해석 후 연결된 명령을 실행한다. */
 
         output.println();
-        output.println("application is ready.");
+        output.println(messageSource.getMessage("application.ready", new Object[0], Locale.getDefault()));
 
         // quit(애플리케이션 종료) 명령어가 입력되기 전까지 무한히 반복하기(infinite loop)
         while (running.get()) {
@@ -126,7 +140,19 @@ public class MovieBuddyApplication {
                 }
                 commandAction.accept(arguments);
             } catch (ApplicationException error) {
-                output.println(error.getMessage());
+//            	if(error instanceof ApplicationException.CommandNotFoundException) {
+//            		output.println(messageSource.getMessage("application.error.commandNotFoundError", new Object[0], Locale.getDefault()));
+//            	} else if(error instanceof ApplicationException.UndefinedCommandActionException) {
+//            		
+//            	} else if(error instanceof ApplicationException.InvalidCommandArgumentsException) {
+//            		
+//            	} else {
+//            		
+//            	}
+            	
+            	// application.errors ~
+            	String code = String.format("application.errors.%s", error.getClass().getSimpleName());
+            	output.println(messageSource.getMessage(code, new Object[0], error.getMessage(), Locale.getDefault()));
             } finally {
                 output.flush();
             }
